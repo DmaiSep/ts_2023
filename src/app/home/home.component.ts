593008b7bfa4df8e55334f6f9c7c62abdc063598
Raulyoyo <raul.dmai@gmail.com>
2023-05-17 13:31:06 -0600

    modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MDBModalRef, MDBModalService } from 'ng-uikit-pro-standard';
+import { ModaldiComponent } from '../modaldi/modaldi.component';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  modalRef: MDBModalRef;
+
+  constructor(private modalService: MDBModalService) { }
+
+  openModal() {
+    this.modalRef = this.modalService.show(ModaldiComponent, {
+        backdrop: true,
+        keyboard: true,
+        focus: true,
+        show: false,
+        ignoreBackdropClick: false,
+        class: 'modal-dialog modal-lg modal-top modal-notify  modal-success modal-dialog-scrollable ldim',
+        containerClass: '',
+        animated: true
+    });
+  }
 
 }
