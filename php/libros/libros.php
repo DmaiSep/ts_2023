f8a419ba9cc6592c08de036ba911bb9a36d3bc7f
Raulyoyo <raul.dmai@gmail.com>
2023-05-18 12:58:16 -0600

    query libros

--- php/libros/libros.php
+++ php/libros/libros.php
@@ -7,12 +7,12 @@ header('content-type: application/json; charset=utf-8');
 
 include '../conecta.php';
 
-$q = "SELECT cat_perfil.id_perfil, cat_grado.id_grado, cat_asignatura.id_asignatura, lib_libros.id_libro, lib_libros.titulo_libro, lib_libros.ver_libro, lib_libros.img_libro, lib_libros.estado 
-FROM (((lib_libros 
-INNER JOIN cat_perfil ON cat_perfil.id_perfil = lib_libros.id_perfil) 
-INNER JOIN cat_grado ON cat_grado.id_grado = lib_libros.id_grado) 
-INNER JOIN cat_asignatura ON cat_asignatura.id_asignatura = lib_libros.id_asignatura) WHERE estado=1
-ORDER BY rand();";
+$q = "SELECT cat_perfil.id_perfil, cat_grado.id_grado, cat_asignatura.id_asignatura, lib_libros.id_libro, lib_libros.titulo_libro, lib_libros.ver_libro, 
+      lib_libros.img_libro, lib_libros.zip_libro , lib_libros.estado 
+	  FROM (((lib_libros 
+	  INNER JOIN cat_perfil ON cat_perfil.id_perfil = lib_libros.id_perfil) 
+	  INNER JOIN cat_grado ON cat_grado.id_grado = lib_libros.id_grado) 
+	  INNER JOIN cat_asignatura ON cat_asignatura.id_asignatura = lib_libros.id_asignatura) WHERE estado=1 ORDER BY rand();";
 
 $conn	=	conecta_bd();
 $result = mysqli_query($conn,$q);
