4965b99848a3e99ace67de7f40daaf94e49dffd4
Raulyoyo <raul.dmai@gmail.com>
2023-05-25 12:21:23 -0600

    Update material_sugerido.php

--- php/material_sugerido/material_sugerido.php
+++ php/material_sugerido/material_sugerido.php
@@ -8,7 +8,7 @@ header('content-type: application/json; charset=utf-8');
 include '../conecta.php';
 
   $q = "(
-	(SELECT cat_grado.grado, cat_asignatura.asignatura, null AS id_bloque, null AS secuencia, 'Libro' AS tipo_material,  1 AS id_material, lib_libros.titulo_libro AS titulo ,lib_libros.img_libro AS img, lib_libros.ver_libro as url, lib_libros.zip_libro AS exe, null AS orden
+	(SELECT  lib_libros.id_asignatura,lib_libros.id_grado , lib_libros.id_perfil, null AS orden, cat_grado.grado, cat_asignatura.asignatura, null AS id_bloque, null AS secuencia, 'Libro' AS tipo_material,  1 AS id_material, lib_libros.titulo_libro AS titulo ,lib_libros.img_libro AS img, lib_libros.ver_libro as url, lib_libros.zip_libro AS exe, null AS orden
 		FROM ( 
 						( lib_libros LEFT JOIN cat_grado ON cat_grado.id_grado = lib_libros.id_grado) 
 						LEFT JOIN cat_asignatura ON cat_asignatura.id_asignatura = lib_libros.id_asignatura
@@ -21,7 +21,7 @@ include '../conecta.php';
 UNION
 
 (
-	SELECT cat_grado.grado,  cat_asignatura.asignatura, cat_bloque.id_bloque AS id_bloque,  null AS secuencia, 'Informatico' AS tipo_material, 2 AS id_material, lib_materiales_informaticos.nom_material AS titulo, lib_materiales_informaticos.img AS img , lib_materiales_informaticos.url AS url, lib_materiales_informaticos.exe AS exe, null AS orden
+	SELECT   lib_materiales_informaticos.id_asignatura, lib_materiales_informaticos.id_grado, lib_materiales_informaticos.id_perfil, null AS orden, cat_grado.grado,  cat_asignatura.asignatura, cat_bloque.id_bloque AS id_bloque,  null AS secuencia, 'Informatico' AS tipo_material, 2 AS id_material, lib_materiales_informaticos.nom_material AS titulo, lib_materiales_informaticos.img AS img , lib_materiales_informaticos.url AS url, lib_materiales_informaticos.exe AS exe, null AS orden
 		FROM  (  
 						(  
 							( lib_materiales_informaticos LEFT JOIN cat_grado ON cat_grado.id_grado = lib_materiales_informaticos.id_grado) 
@@ -35,7 +35,7 @@ UNION
 UNION 
 
 (
-	SELECT  cat_grado.grado, cat_asignatura.asignatura,  cat_bloque.id_bloque  AS id_bloque,  secuencia AS secuencia, 'Audiovisual' AS tipo_material, 3 AS id_material, lib_mat_audiovisuales.nom_video AS titulo , null AS img, lib_mat_audiovisuales.url_video_normal AS url, 
+	SELECT  lib_mat_audiovisuales.id_asignatura ,lib_mat_audiovisuales.id_grado, lib_mat_audiovisuales.id_perfil, lib_mat_audiovisuales.orden, cat_grado.grado, cat_asignatura.asignatura,  cat_bloque.id_bloque  AS id_bloque,  secuencia AS secuencia, 'Audiovisual' AS tipo_material, 3 AS id_material, lib_mat_audiovisuales.nom_video AS titulo , null AS img, lib_mat_audiovisuales.url_video_normal AS url, 
 	lib_mat_audiovisuales.url_zip_normal AS exe, lib_mat_audiovisuales.orden AS orden
 	FROM (
 					(	  
@@ -48,7 +48,6 @@ UNION
 )
 ORDER BY rand()
 LIMIT 30;
-
 ";
  
   $conn	=	conecta_bd();
